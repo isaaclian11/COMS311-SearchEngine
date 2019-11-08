739747b035e180dd11b3e980587a45f1d35b2e73
isanga <isanga@iastate.edu>
2019-11-08 13:02:41 -0600

    Mostly finished with graph

--- src/pa1/Crawler.java
+++ src/pa1/Crawler.java
@@ -1,15 +1,28 @@
 package pa1;
 
 import api.Graph;
+import api.TaggedVertex;
+import api.Util;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Queue;
 
 /**
  * Implementation of a basic web crawler that creates a graph of some
  * portion of the world wide web.
  *
- * @author PLEASE FILL IN TEAM MEMBER NAMES HERE
+ * @author Isaac Sanga, Justin Worley
  */
 public class Crawler
 {
+  private String seedUrl;
+  private int maxDepth, maxPages;
   /**
    * Constructs a Crawler that will start with the given seed url, including
    * only up to maxPages pages at distance up to maxDepth from the seed url.
@@ -19,7 +32,9 @@ public class Crawler
    */
   public Crawler(String seedUrl, int maxDepth, int maxPages)
   {
-    // TODO
+    this.seedUrl = seedUrl;
+    this.maxDepth = maxDepth;
+    this.maxPages = maxPages;
   }
   
   /**
@@ -29,9 +44,59 @@ public class Crawler
    * @return
    *   an instance of Graph representing this portion of the web
    */
-  public Graph<String> crawl()
-  {
+  public Graph<String> crawl() throws IOException {
     // TODO
-    return null;
+
+    WebGraph webGraph = new WebGraph(maxPages);
+    if(maxPages==0){
+      return webGraph;
+    }
+
+    HashSet<String> visited = new HashSet();
+    visited.add(seedUrl);
+
+    Queue<String> queue = new LinkedList();
+    queue.add(seedUrl);
+
+    int count = 0;
+    int layer = 0;
+    int requestCount = 0;
+
+    while(!queue.isEmpty() && layer<=maxDepth) {
+      String url = queue.poll();
+      if(requestCount<=50) {
+        requestCount=0;
+        try {
+          Thread.sleep(3000);
+        } catch (InterruptedException e) {
+          e.printStackTrace();
+        }
+      }
+      Document doc = Jsoup.connect(url).get();
+      requestCount++;
+      Elements elements = doc.select("a[href]");
+      TaggedVertex parent = new TaggedVertex(url, count);
+      count++;
+      for (Element link : elements) {
+        String v = link.attr("abs:href");
+        if(!Util.ignoreLink(url, v) && !visited.contains(v)){
+          visited.add(v);
+          TaggedVertex vertex = new TaggedVertex(v, count);
+          webGraph.addEdge(parent, vertex);
+          queue.add(v);
+          count++;
+        }
+        if(count==maxPages)
+          return webGraph;
+      }
+      layer++;
+      if(layer==maxDepth)
+        return webGraph;
+    }
+
+    return webGraph;
   }
+
+
+
 }
---
+++ src/pa1/WebGraph.java
@@ -0,0 +1,94 @@
+package pa1;
+
+import api.Graph;
+import api.TaggedVertex;
+
+import java.util.*;
+
+public class WebGraph implements Graph {
+
+    private HashMap<Integer, ArrayList<TaggedVertex>> adjList = new HashMap<>();
+    private HashMap<Integer, ArrayList<TaggedVertex>> incoming = new HashMap<>();
+    private HashSet<TaggedVertex> vertices = new HashSet<>();
+
+    public WebGraph(int size){
+        for(int i=0; i<size; i++){
+            adjList.put(i, new ArrayList<>());
+            incoming.put(i, new ArrayList<>());
+        }
+    }
+
+    public void addEdge(TaggedVertex start, TaggedVertex end){
+        if(start.getTagValue()>=adjList.size() || end.getTagValue()>=adjList.size()){
+            return;
+        }
+        if(!vertices.contains(start)) {
+            ArrayList adj = adjList.get(start.getTagValue());
+            ArrayList in = incoming.get(start.getTagValue());
+            adj.add(start);
+            in.add(start);
+            adjList.put(start.getTagValue(), adj);
+            incoming.put(start.getTagValue(), in);
+            vertices.add(start);
+        }
+        if(!vertices.contains(end)) {
+            ArrayList adj = adjList.get(end.getTagValue());
+            ArrayList in = incoming.get(end.getTagValue());
+            adj.add(end);
+            in.add(end);
+            adjList.put(end.getTagValue(), adj);
+            incoming.put(end.getTagValue(), in);
+            vertices.add(end);
+        }
+        adjList.get(start.getTagValue()).add(end);
+        incoming.get(end.getTagValue()).add(start);
+    }
+
+    @Override
+    public ArrayList vertexData() {
+        ArrayList<Object> data = new ArrayList<>();
+        for(int i=0; i<adjList.size(); i++){
+            if(adjList.get(i).size()>0)
+             data.add(adjList.get(i).get(0).getVertexData());
+        }
+        return data;
+    }
+
+    @Override
+    public ArrayList<TaggedVertex> vertexDataWithIncomingCounts() {
+        ArrayList<TaggedVertex> list = new ArrayList<>();
+        for(int i=0; i<incoming.size(); i++){
+            if(incoming.get(i).size()>0) {
+                Object data = incoming.get(i).get(0).getVertexData();
+                int value = incoming.get(i).size() - 1;
+                TaggedVertex vertex = new TaggedVertex(data, value);
+                list.add(vertex);
+            }
+        }
+        return list;
+    }
+
+    @Override
+    public List<Integer> getNeighbors(int index) {
+        List<Integer> list = new ArrayList<>();
+        if(adjList.get(index).size()==0)
+            return list;
+        ArrayList<TaggedVertex> vertices = adjList.get(index);
+        for(int i=1; i<vertices.size(); i++){
+            list.add(vertices.get(i).getTagValue());
+        }
+        return list;
+    }
+
+    @Override
+    public List<Integer> getIncoming(int index) {
+        List<Integer> list = new ArrayList<>();
+        if(incoming.get(index).size()==0)
+            return list;
+        ArrayList<TaggedVertex> vertices = incoming.get(index);
+        for(int i=1; i<vertices.size(); i++){
+            list.add(vertices.get(i).getTagValue());
+        }
+        return list;
+    }
+}
